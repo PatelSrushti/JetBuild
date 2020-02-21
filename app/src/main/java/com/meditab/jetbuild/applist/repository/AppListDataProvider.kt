3a27d67508828d94e76863908f722c556ebdfe0f
Srushti <p.srushti91@gmail.com>
2020-02-21 18:27:44 +0530

    show app list data from firebase

--- app/src/main/java/com/meditab/jetbuild/applist/repository/AppListDataProvider.kt
+++ app/src/main/java/com/meditab/jetbuild/applist/repository/AppListDataProvider.kt
@@ -1,13 +1,14 @@
 package com.meditab.jetbuild.applist.repository
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.meditab.jetbuild.applist.datamodel.AppData
 import com.meditab.jetbuild.firebase.AppDatabase
 
 class AppListDataProvider {
-    fun getAppList(): MutableLiveData<List<AppData>> {
-        AppDatabase().getAppListData()
-        return getList()
+    fun getAppList(): LiveData<List<AppData>> {
+        return AppDatabase().getAppListData()
+//        return getList()
     }
 
     private fun getList(): MutableLiveData<List<AppData>> {
