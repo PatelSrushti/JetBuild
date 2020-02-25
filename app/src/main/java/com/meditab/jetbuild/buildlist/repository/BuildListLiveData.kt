f7341416d07a4681898dfd6ece0219fc0ad6b704
Mayur Dhanrajani <mayurd@meditab.com>
2020-02-25 17:19:11 +0530

    Removed the build when expired.

--- app/src/main/java/com/meditab/jetbuild/buildlist/repository/BuildListLiveData.kt
+++ app/src/main/java/com/meditab/jetbuild/buildlist/repository/BuildListLiveData.kt
@@ -1,10 +1,11 @@
 package com.meditab.jetbuild.buildlist.repository
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import com.meditab.jetbuild.buildlist.datamodel.BuildData
+import com.meditab.jetbuild.firebase.toValues
+import java.util.concurrent.TimeUnit
 
 class BuildListLiveData(private val reference: DatabaseReference) :
     MutableLiveData<DataSnapshot>() {
@@ -22,6 +23,30 @@ class BuildListLiveData(private val reference: DatabaseReference) :
     inner class MyValueEventListener : ValueEventListener {
 
         override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+            dataSnapshot.let {
+                val genericTypeIndicator =
+                    object :
+                        GenericTypeIndicator<Map<String, BuildData>?>() {}
+
+                val apps = dataSnapshot.getValue(genericTypeIndicator)
+                val list = apps?.toList().toValues()
+
+                list.forEach {
+
+                    val diff = TimeUnit.DAYS.convert(
+                        it.expiryDate - System.currentTimeMillis(),
+                        TimeUnit.MILLISECONDS
+                    )
+
+                    if (diff.toInt() == 0) {
+                        reference.child(it.id).removeValue()
+                    }
+
+                }
+
+            }
+
             value = dataSnapshot
         }
 
